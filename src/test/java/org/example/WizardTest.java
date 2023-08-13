b09308cdc324054032896c74a463066b39fd786e
a0935210570602 <a0935210570602@gmail.com>
2023-08-13 18:51:47 +0800

    Add annotation

--- src/test/java/org/example/WizardTest.java
+++ src/test/java/org/example/WizardTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class WizardTest {
     @Test
     public void testWizardNameHealthSpells() {
+        // Step 1-3
         // 測試一個新的巫師 instance 被成功建立。
         // 驗證標準：確保巫師的名字和生命值被正確地設定，並且他開始時沒有任何法術。
         // Testing the initialization of a new wizard object.
@@ -15,6 +16,7 @@ class WizardTest {
 
     @Test
     public void testWizardLearnSpell() {
+        // Step 4-6
         // 測試巫師學習新法術。
         // 驗證標準：確保法術被正確地添加到巫師的法術列表中。
         // Testing the functionality of a wizard learning a new spell.
@@ -23,6 +25,7 @@ class WizardTest {
 
     @Test
     public void testWizardCastSpell() {
+        // Step 7-9
         // 測試一名巫師對另一名巫師施放法術的功能。
         // 驗證標準：確保目標巫師的生命值正確地減少。
         // Testing the functionality of a wizard casting a spell on another wizard.
@@ -31,6 +34,7 @@ class WizardTest {
 
     @Test
     public void testWizardHealthDecreaseAfterBeingHit() {
+        // Step 10-12
         // 測試當巫師被法術擊中時，他的生命值應該減少。
         // 驗證標準：確保巫師的生命值正確地減少。
         // Testing how a wizard's health should decrease when hit by a spell.
@@ -39,6 +43,7 @@ class WizardTest {
 
     @Test
     public void testWizardDuelTournament() {
+        // Step 13-15
         // 測試一場巫師之間的對決。
         // 驗證標準：確保每個巫師施放的法術都正確地影響了對方的生命值。
         // Testing a duel between wizards.
